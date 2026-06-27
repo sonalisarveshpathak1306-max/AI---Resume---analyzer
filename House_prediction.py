0f1564f0cf70399a97388564fd205afed725849b
sonalisarveshpathak1306-max <sonalisarveshpathak1306@gmail.com>
2026-06-27 21:53:11 +0530

    Update and rename Housing.csv 2.csv to House_prediction.py

--- House_prediction.py
+++ House_prediction.py
@@ -1,4 +1,4 @@
-import pandas as pd
+
 df = pd.read_csv('Housing.csv')
 print(df.head())
 
@@ -19,12 +19,9 @@ print(df.head())
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-
 X = df.drop('price', axis=1)
 y = df['price']
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
 model = LinearRegression()
 model.fit(X_train, y_train)
 
